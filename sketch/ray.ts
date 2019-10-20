2287f5057df951d742ddc0cc0edd4da1156362a7
Jeppe Enslev <jeppe.enslev@gmail.com>
2019-10-21 00:49:00 +0200

    initial commit

---
+++ sketch/ray.ts
@@ -0,0 +1,62 @@
+class Ray {
+    pos: p5.Vector;
+    dir: number;
+
+    constructor(x: number, y: number, dir: number) {
+        this.pos = createVector(x, y);
+        this.dir = dir;
+    }
+
+    // Return intersection point between THIS ray and all walls as vector.
+    cast(): p5.Vector {
+        const intersection =  walls
+            .map(wall => this.castHelper(wall))
+            .filter(x => x)
+            .reduce((acc, cur) => {
+                if (!acc) {
+                    return cur;
+                }
+
+                if (cur.t < acc.t) {
+                    return cur
+                }
+                return acc;
+            }, undefined)
+
+        return intersection ? intersection.point : undefined;
+    }
+
+    // Return intersection point between THIS ray and given wall as vector.
+    private castHelper(wall: Boundary) {
+        const x1 = this.pos.x;
+        const y1 = this.pos.y;
+        const x2 = x1 + Math.cos(this.dir);
+        const y2 = y1 + Math.sin(this.dir);
+        const x3 = wall.a.x;
+        const y3 = wall.a.y;
+        const x4 = wall.b.x;
+        const y4 = wall.b.y;
+
+        const det = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+        if (det === 0) {
+            return;
+        }
+
+        const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / det;
+        const u = -(((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / det);
+
+        if (t >= 0 && u >= 0 && u <= 1) {
+            return {
+                t,
+                u,
+                point: createVector(
+                    x1 + t * (x2 - x1),
+                    y1 + t * (y2 - y1)
+                ),
+            }
+        }
+
+        return;
+    }
+}
---
+++ sketch/sketch.ts
@@ -0,0 +1,37 @@
+
+let walls: Boundary[] = [];
+let caster: Caster;
+let locked: boolean = false;
+
+function setup() {
+    createCanvas(500, 500)
+
+    caster = new Caster(500);
+
+    // Bounderies of canvas
+    walls.push(new Boundary(-1, -1, 501, -1));
+    walls.push(new Boundary(501, -1, 501, 501));
+    walls.push(new Boundary(501, 501, -1, 501));
+    walls.push(new Boundary(-1, 501, -1, -1));
+
+
+    walls.push(new Boundary(250, 200, 100, 450));
+    walls.push(new Boundary(250, 200, 300, 200));
+    walls.push(new Boundary(350, 100, 450, 400));
+}
+
+function mousePressed() {
+    locked = !locked;
+}
+
+function draw() {
+    background(0);
+
+    walls.forEach(wall => wall.show());
+
+    if (!locked) {
+        caster.setPos(mouseX, mouseY);
+    }
+
+    caster.full();
+}
